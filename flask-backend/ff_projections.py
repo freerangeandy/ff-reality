67ccad2f331a3b3c61af5c27cc7b96e046c96053
freerangeandy <freerangeandy@gmail.com>
2020-10-19 14:26:58 -0400

    add (actual) get_rankings function

--- flask-backend/ff_projections.py
+++ flask-backend/ff_projections.py
@@ -1,6 +1,7 @@
 import requests
 import lxml.html as lh
 import pandas
+from bs4 import BeautifulSoup
 
 # JS script that builds rankings table:
 # https://g.espncdn.com/lm-static/ffl/tools/rankingsTable.js?slotCategoryId=0&scoringPeriodId=5&seasonId=2020&rankType=ppr&count=25&rand=2
@@ -35,4 +36,26 @@ def get_projections():
     df = df.drop("Rank, Player",1)
     cols = df.columns
     df = df[cols[-2:].append(cols[:-2])]
+    return df.to_html()
+
+def get_rankings():
+    l = []
+    base_url = "https://www.fantasypros.com/nfl/reports/leaders/qb.php?year=2020&start=4&end=4"
+    r = requests.get(base_url,
+                 headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    all_tr = soup.find("tbody").find_all("tr")
+    rankings = []
+    for tr in all_tr:
+        center_tds = tr.find_all("td", {"class":"center"})
+        rank = center_tds[0].text
+        team =  center_tds[1].text
+        score = center_tds[2].text
+        player = tr.find("a", {"class":"player-name"}).text
+        player_team = "%s, %s" % (player, team)
+        rankings.append([rank, player_team, score])
+    df = pandas.DataFrame(
+            data=rankings,
+            columns=["Ranking","Player","Score"])
     return df.to_html()
