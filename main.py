d984c7934300db9421ba09e5a3f71cf4648d8560
freerangeandy <freerangeandy@gmail.com>
2020-11-12 18:06:37 -0500

    allow form submission

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from ff_projections import positions, analysts, get_comparison, get_full_comparison
+
+position_list = [(num, position.upper()) for num, position in enumerate(positions)]
+analyst_list = [(num, analyst) for num, analyst in enumerate(analysts)]
 
 app = Flask(__name__)
 
 @app.route('/')
-def my_index():
-    position_list = [(num, position.upper()) for num, position in enumerate(positions)]
-    analyst_list = [(num, analyst) for num, analyst in enumerate(analysts)]
-
+def index():
     return render_template("index.html",
         weeks=range(1,8),
         positions=position_list,
         analysts=analyst_list
     )
 
-@app.route('/success')
+@app.route('/success', methods=['POST'])
 def success():
-    df_html = get_projections()
-    return render_template("index.html",
-        table= None
-    )
+    if request.method == 'POST':
+        week_no = int(request.form['weeks-select'])
+        position_idx = int(request.form['positions-select'])
+        analyst_idx = int(request.form['analysts-select'])
+
+
+        return render_template("index.html",
+            weeks=range(1,8),
+            positions=position_list,
+            analysts=analyst_list,
+            error=f"Week: {week_no}, Position: {position_list[position_idx][1]}, Analyst: {analyst_list[analyst_idx][1]}"
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
